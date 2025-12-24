aa84295da6c07c6c7d77a941ef6ec6a87bc9022b
Chirag <prajapatichirag1122@gmail.com>
2025-12-24 19:26:51 +0530

    delete exisiting migration which is in use on local db

--- arna.HRMS.Infrastructure/Data/Identity/ApplicationRole.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace arna.HRMS.Infrastructure.Data.Identity;
-
-public class ApplicationRole : IdentityRole<int>
-{
-    public string Description { get; set; }
-}
