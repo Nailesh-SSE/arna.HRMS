2ee7b2b27619878bdd8a827136a5e4a18026cdb2
nagrawal <nagrawal@triage-partners.com>
2026-01-10 17:31:55 +0530

    refactor the code

---
+++ arna.HRMS/Components/Common/Tables/TableHeader.cs
@@ -0,0 +1,9 @@
+﻿namespace arna.HRMS.Components.Common.Tables;
+
+public class TableHeader
+{
+    public string Title { get; set; } = string.Empty;
+    public string? CssClass { get; set; }
+    public bool Hidden { get; set; } = false;
+    public bool NoWrap { get; set; } = true;
+}
