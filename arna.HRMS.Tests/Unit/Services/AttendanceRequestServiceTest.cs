4396671b9aa73ab23c9d0fdf0498d2f0c5d2506b
Aryan Parikh <prkharyan@gmail.com>
2026-02-10 12:00:10 +0530

    resolve conflict

--- arna.HRMS.Tests/Unit/Services/AttendanceRequestServiceTest.cs
+++ arna.HRMS.Tests/Unit/Services/AttendanceRequestServiceTest.cs
@@ -541,7 +541,6 @@ public class AttendanceRequestServiceTests
 
         await _dbContext.SaveChangesAsync();
 
-        var result = await _service.GetAttendanceRequests(2, Status.Pending);
         var result = await _service.GetAttendanceRequests(2, null);
 
         Assert.That(result.IsSuccess, Is.True);
