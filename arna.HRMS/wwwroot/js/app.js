2f41fc46a2a0efbbaab4c5674338fc78e6697a9f
nagrawal <nagrawal@triage-partners.com>
2026-02-24 12:15:47 +0530

    init UI changes

--- arna.HRMS/wwwroot/js/app.js
+++ arna.HRMS/wwwroot/js/app.js
@@ -1,1 +1,12 @@
-﻿
+﻿window.hrmsSetHeaderHeight = function () {
+    var header = document.getElementById('hrms-header');
+    if (header) {
+        var h = header.getBoundingClientRect().height;
+        document.documentElement.style.setProperty('--hrms-header-h', h + 'px');
+    }
+};
+
+window.hrmsSetHeaderHeight();
+window.addEventListener('resize', function () {
+    window.hrmsSetHeaderHeight();
+});
